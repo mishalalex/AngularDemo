765a3316aec3a855e5db6cc6dfbda8b4934aeb8b
Mishal Alexander <mishalalexander20@gmail.com>
2021-11-26 00:25:14 +0530

    logic to retrieve & update product price on cart page

--- src/app/cart-items/cart-items.component.ts
+++ src/app/cart-items/cart-items.component.ts
@@ -9,10 +9,31 @@ import { CartService } from '../cart.service';
 export class CartItemsComponent implements OnInit {
   
   products = this.cart.getProducts();
+  sum: number;
+  newPrice: number;
+  estimatedTax: number = 8;
+  totalPrice: number;
   
   constructor(private cart: CartService) { }
 
   ngOnInit(): void {
+    // logic for calculating sub total at page initialization
+    this.sum = 0;
+    for (let i=0; i<this.products.length; i++) {
+      this.sum += this.products[i].price;
+    }
+
+    // adding estimated tax for total
+    this.totalPrice = this.sum + this.estimatedTax;
+  }
+
+  updateQuantity(productPrice:number, quantity:number) {
+      //product.price = original price
+      //product price * quantity = new price
+      // sum - original price = new price
+      this.newPrice = productPrice * quantity;
+      this.sum = this.sum - productPrice + this.newPrice;
+      this.totalPrice = this.sum + this.estimatedTax;
   }
 
 }
